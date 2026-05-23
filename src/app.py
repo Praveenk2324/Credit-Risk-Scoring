84735edf54987e3820fcc94366edd3593c699a97
Praveenk2324 <praveennithishan2005@gmail.com>
2026-05-23 16:12:21 +0530

    Wired Streamlit to AWS Fargate IP

--- src/app.py
+++ src/app.py
@@ -47,7 +47,7 @@ if st.button("Predict Risk", type="primary", use_container_width=True):
         income_missing, dependents_missing
     ]
     
-    API_URL = "http://127.0.0.1:8000/predict"
+    API_URL ="http://16.16.196.24:8000/predict"
     payload = {"features": raw_array}
     
     with st.spinner("Consulting the Docker API..."):
